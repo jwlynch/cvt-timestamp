f9d1a4c07e32af13a467284340d61f05a2f3af8d
Jim <jim@jam.sessionsnet.org>
2019-07-21 15:44:13 -0700

    get rid of (commented) print call involving datetime module

--- cvtdate.py
+++ cvtdate.py
@@ -2,8 +2,6 @@
 
 import pendulum
 
-#print(strptime("2019-07-15 16:10:18Z", "%Y-%m-%d %H:%M:%S"))
-
 tim = pendulum.parse("2019-07-15 16:10:18Z")
 
 print(tim.astimezone(pendulum.local_timezone()))
