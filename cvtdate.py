6e0b52a0345a6f561539e4b7b2e3bd2873e84eae
Jim <jim@jam.sessionsnet.org>
2019-07-21 16:08:10 -0700

    expand print call to also print numArgs

--- cvtdate.py
+++ cvtdate.py
@@ -6,7 +6,7 @@ import pendulum
 timeArg = sys.argv[0]
 numArgs = len(sys.argv)
 
-print(timeArg)
+print("timearg %s, numArgs %s\n" % (timeArg, str(numArgs)))
 
 tim = pendulum.parse("2019-07-15 16:10:18Z")
 
