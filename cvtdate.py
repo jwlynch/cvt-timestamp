4691a12e5e190f4088bfc252f8e3690c5dc840be
Jim <jim@jam.sessionsnet.org>
2019-07-21 16:21:08 -0700

    add simple arg parser

--- cvtdate.py
+++ cvtdate.py
@@ -6,6 +6,13 @@ import pendulum
 timeArg = "(none)"
 numArgs = len(sys.argv)
 
+if numArgs < 2:
+    print("missing time arg")
+elif numArgs > 2:
+    print("too many args")
+else:
+    timeArg = sys.argv[1]
+
 print("timearg %s, numArgs %s\n" % (timeArg, str(numArgs)))
 
 tim = pendulum.parse("2019-07-15 16:10:18Z")
